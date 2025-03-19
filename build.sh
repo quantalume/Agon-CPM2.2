3078e10472281129a3acff3b05cabaa469d2f3ac
quantalume <quantalume@gmail.com>
2025-03-19 15:31:12 -0500

    Fix build.sh

--- build.sh
+++ build.sh
@@ -16,7 +16,6 @@ cp bootstrap/cpm.bin cpm.bin
 # ----- Building for CON:=TTY: (UART1) -----
 # On your PC
 cd sources && sjasmplus main.asm
-cd bootstrap && ez80asm cpm.asm
 
 # On the Agon or emulator
 (copy bootstrap folder from PC to Agon)
